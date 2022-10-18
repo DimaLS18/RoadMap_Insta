2999c1006f413702ec3f183d4f74f09570784225
DimaLS18 <kovrigindima@gmail.com>
2022-10-18 14:32:06 +0300

    init

---
+++ RoadMap_Insta/RoadMap_Insta/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RoadMap_Insta
+//
+//  Created by Dima Kovrigin on 18.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
